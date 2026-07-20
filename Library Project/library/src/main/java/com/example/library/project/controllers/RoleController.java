910654b0104aba7da13b9328465f866a4ba6fc69
Bahram Teymoori <70304284+bahramteymoori1994@users.noreply.github.com>
2026-07-20 20:20:13 +0330

    Role form created

---
+++ Library Project/library/src/main/java/com/example/library/project/controllers/RoleController.java
@@ -0,0 +1,62 @@
+package com.example.library.project.controllers;
+
+import com.example.library.project.dto.requests.RoleRequestDto;
+import com.example.library.project.dto.responses.RoleResponseDto;
+import com.example.library.project.services.interfaces.RoleService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import java.util.List;
+
+@Controller
+@RequestMapping("role")
+public class RoleController {
+
+    private final RoleService roleService;
+
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @PostMapping("/saveRole")
+    public String save(@ModelAttribute("roleDto") RoleRequestDto roleRequestDto, RedirectAttributes redirectAttributes) throws Exception {
+
+        try{
+
+            roleService.save(roleRequestDto);
+            redirectAttributes.addFlashAttribute("message", "نقش کاربر با موفقیت ثبت شد");
+        }
+        catch(Exception exception){
+            redirectAttributes.addFlashAttribute("message", "خطا در ثبت");
+        }
+
+        return "redirect:/role";
+    }
+
+    @PutMapping("/updateRole")
+    public RoleResponseDto update(@RequestBody RoleRequestDto roleRequestDto) throws Exception {
+        return roleService.update(roleRequestDto);
+    }
+
+    @GetMapping("/findRoleById/{id}")
+    public RoleResponseDto findRoleById(@PathVariable Long id){
+        return roleService.findById(id);
+    }
+
+    @GetMapping("/findAllRoles")
+    public List<RoleResponseDto> findAllRoles(){
+        return roleService.findAll();
+    }
+
+    @GetMapping()
+    public String getAllRoles(Model model){
+
+        List<RoleResponseDto> roles = findAllRoles();
+
+        model.addAttribute("roles", roles);
+        model.addAttribute("roleDto", new RoleRequestDto());
+
+        return "role";
+    }
+}
--- Library Project/library/src/main/java/com/example/library/project/controllers/UserController.java
+++ Library Project/library/src/main/java/com/example/library/project/controllers/UserController.java
@@ -1,38 +1,35 @@
 package com.example.library.project.controllers;
 
-import com.example.library.project.dto.requests.RoleRequestDto;
 import com.example.library.project.dto.requests.UserRequestDto;
 import com.example.library.project.dto.responses.PersonResponseDto;
-import com.example.library.project.dto.responses.RoleResponseDto;
 import com.example.library.project.dto.responses.UserResponseDto;
 import com.example.library.project.dto.views.UserViewResponseDto;
 import com.example.library.project.services.interfaces.PersonService;
-import com.example.library.project.services.interfaces.RoleService;
 import com.example.library.project.services.interfaces.UserService;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
     private final PersonService personService;
-    private final RoleService roleService;
 
-    public UserController(UserService userService, PersonService personService, RoleService roleService) {
+    public UserController(UserService userService, PersonService personService) {
         this.userService = userService;
         this.personService = personService;
-        this.roleService = roleService;
     }
 
     @GetMapping
     public String getAllUsers(Model model) {
         List<UserViewResponseDto> users = userService.findUsersView();
+
         model.addAttribute("users", users);
         model.addAttribute("userDto", new UserRequestDto());
         return "user";
@@ -75,25 +72,5 @@ public class UserController {
     @ResponseStatus(value = HttpStatus.OK)
     public List<PersonResponseDto> findAllPeople(){
         return personService.findAll();
-    }
-
-    @PostMapping("/role/saveRole")
-    public RoleResponseDto save(@RequestBody RoleRequestDto roleRequestDto) throws Exception {
-        return roleService.save(roleRequestDto);
-    }
-
-    @PutMapping("/role/updateRole")
-    public RoleResponseDto update(@RequestBody RoleRequestDto roleRequestDto) throws Exception {
-        return roleService.update(roleRequestDto);
-    }
-
-    @GetMapping("/role/findRoleById/{id}")
-    public RoleResponseDto findRoleById(@PathVariable Long id){
-        return roleService.findById(id);
-    }
-
-    @GetMapping("/role/findAllRoles")
-    public List<RoleResponseDto> findAllRoles(){
-        return roleService.findAll();
     }
 }
