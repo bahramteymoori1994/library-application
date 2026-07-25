ec5f7d35331ec66ec80e05eaf2be1174b24027cd
Bahram Teymoori <70304284+bahramteymoori1994@users.noreply.github.com>
2026-07-25 21:40:00 +0330

    Add docker and redis dependencies

--- Library Project/library/src/main/java/com/example/library/project/controllers/RoleController.java
+++ Library Project/library/src/main/java/com/example/library/project/controllers/RoleController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/role")
 public class RoleController {
 
