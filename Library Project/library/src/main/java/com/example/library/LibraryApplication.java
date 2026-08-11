5be760faa2a32762f97eeafd62173b5bc326dd59
Bahram Teymoori <70304284+bahramteymoori1994@users.noreply.github.com>
2026-08-11 23:20:20 +0330

    All passwords have been encrypted in users

--- Library Project/library/src/main/java/com/example/library/LibraryApplication.java
+++ Library Project/library/src/main/java/com/example/library/LibraryApplication.java
@@ -1,42 +1,14 @@
 package com.example.library;
 
-import com.example.library.project.dto.responses.UserResponseDto;
-import com.example.library.project.services.interfaces.UserService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import java.util.List;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableCaching
-public class LibraryApplication implements CommandLineRunner {
-
-    public static UserService userService;
-    private BCryptPasswordEncoder bCryptPasswordEncoder = new  BCryptPasswordEncoder();
-
-    public LibraryApplication(UserService userService) {
-        LibraryApplication.userService = userService;
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-
-        List<UserResponseDto> findAllUsers = userService.findAll();
-
-        findAllUsers
-                .stream()
-                .forEach((user ->
-                {
-                    user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-                }));
-
-        System.out.println(findAllUsers);
-    }
+public class LibraryApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryApplication.class, args);
