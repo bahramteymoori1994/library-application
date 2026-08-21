a2091236419aa1f74d5d53a7f61e071274165f0d
Bahram Teymoori <70304284+bahramteymoori1994@users.noreply.github.com>
2026-08-21 13:15:31 +0330

    Clean unnecessary import packages

--- Library Project/library/src/main/java/com/example/library/project/model/entities/User.java
+++ Library Project/library/src/main/java/com/example/library/project/model/entities/User.java
@@ -7,12 +7,10 @@ import org.hibernate.validator.constraints.Length;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 @Entity(name = "userEntity")
